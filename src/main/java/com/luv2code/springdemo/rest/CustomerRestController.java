f4e5ffe918e5016f096edaec28468ec2c068bc23
estrangegit <etienne.estrangin@gmail.com>
2018-10-20 16:10:01 +0200

    66.526 Spring REST - CRUD Database Project - Add Customer

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,10 +29,26 @@ public class CustomerRestController {
 	
 	// Add mapping for Get /customers/{customerId}
 	@GetMapping("/customers/{customerId}")
-	public Customer getCustomer(@PathVariable int customerId) {
+	public Customer getCustomer(@PathVariable int customerId) {		
 		
-		Customer theCustormer = customerService.getCustomer(customerId);
+		Customer theCustomer = customerService.getCustomer(customerId);
 		
-		return theCustormer;
+		if(theCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		return theCustomer;
 	}
+	
+	// add mapping for POST /customers - add new customer
+	@PostMapping("/customers")
+	public Customer addCustomer(@RequestBody Customer theCustomer) {		
+		// Also, just in case the client application pass an id in JSON ... set id to 0
+		// This is to force a save of new item ... instead of update
+		theCustomer.setId(0);
+		
+		customerService.saveCustomer(theCustomer);
+		
+		return theCustomer;
+	}	
 }
